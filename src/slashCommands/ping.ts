8a02f633ad5102715e0d959b1446becccda7d62e
TheProCut <alex.feneck@outlook.com>
2023-11-28 20:59:26 +0000

    Update ping.ts

--- src/slashCommands/ping.ts
+++ src/slashCommands/ping.ts
@@ -1,25 +1,24 @@
-import { SlashCommandBuilder, ChannelType, TextChannel, EmbedBuilder } from "discord.js"
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import { getThemeColor } from "../functions";
 import { SlashCommand } from "../types";
 
-const command : SlashCommand = {
-    command: new SlashCommandBuilder()
+const command: SlashCommand = {
+  command: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Shows the bot's ping")
-    ,
-    execute: interaction => {
-        interaction.reply({
-            embeds: [
-                new EmbedBuilder()
-                .setAuthor({name: "Command Ran: /ping"})
-                .setTitle(`US: License Bot Ping`)
-                .setDescription(`🏓 Pong! \n 📡 Ping: ${interaction.client.ws.ping}`)
-                .setColor(getThemeColor("text"))
-                .setFooter(`Unified Licensing`)
-            ]
-        })
-    },
-    cooldown: 10
-}
+    .setDescription("Shows the bot's ping"),
+  execute: (interaction) => {
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setAuthor({ name: "Command Ran: /ping" })
+          .setTitle(`US: License Bot Ping`)
+          .setDescription(`🏓 Pong! \n 📡 Ping: ${interaction.client.ws.ping}`)
+          .setColor(getThemeColor("text"))
+          .setFooter({ text: "Unified Licensing" }),
+      ],
+    });
+  },
+  cooldown: 10,
+};
 
-export default command
+export default command;
